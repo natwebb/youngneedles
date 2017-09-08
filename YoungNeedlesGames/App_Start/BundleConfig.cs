e2be6e5bc331d49720888777a8fa2ada46795cad
Nat Webb <natrwebb@gmail.com>
2017-09-08 11:28:23 -0400

    testing bundling

--- YoungNeedlesGames/App_Start/BundleConfig.cs
+++ YoungNeedlesGames/App_Start/BundleConfig.cs
@@ -7,12 +7,11 @@ namespace YoungNeedlesGames
     {
         public static void RegisterBundles(BundleCollection bundles)
         {
-            //Vendor Scripts
-            var vendorScripts = new ScriptBundle("~/vendor/scripts").Include(
-                "~/static/node_modules/jquery/dist/jquery.js"
+            var test = new ScriptBundle("~/test/script").Include(
+                "~/static/test.js"
             );
 
-            bundles.Add(vendorScripts);
+            bundles.Add(test);
 
             //Boot Scripts
             var system = new ScriptBundle("~/startup/boot").Include(
